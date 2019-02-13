fa4a4d889449691b310c3c41409d7f3745a6e1cc
Zoe Bai <s.xue.bai@gmail.com>
2019-02-13 21:51:11 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,6 +1,14 @@
 def nyc_pigeon_organizer(data)
   pigeon_list = {}
-  data.collect do |colors, color|
-    color
+ data.collect do |info_type, info|
+    if info_type == :color
+      pigeon_list["Theo"]={}
+      pigeon_list["Theo"][info_type] = []
+     info.collect do |each_color, names|
+      if names.include?("Theo")
+       pigeon_list["Theo"][info_type] << each_color.to_s
+      end
+     end
+    end
   end
 end
